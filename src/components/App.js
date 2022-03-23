c4bcbe95baa7ba83c02c5aaee54fedc8d3c2d63a
Mark Momot <markmomot03@gmail.com>
2022-03-22 22:23:05 -0400

    Added routes, added user info file

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,12 @@ import React, {useState} from "react";
 import { BrowserRouter, Switch , Route } from "react-router-dom"
 import NavBar from "./NavBar"
 import Home from "./Home"
+import Login from "./Login";
+import Signup from "./Signup";
+import NewCar from "./NewCar";
+import NewRepair from "./NewRepair";
+import NewCarOwner from "./NewCarOwner";
+import UserInfo from "./UserInfo";
 
 function App() {
   const [userData, setUserData] = useState({isLoggedIn: false})
@@ -12,7 +18,12 @@ function App() {
       
       <Switch>
         <Route exact path="/"><Home /></Route>
-
+        <Route path="/login"><Login /></Route>
+        <Route path="/signup"><Signup /></Route>
+        <Route path="/cars/new"><NewCar /></Route>
+        <Route path="/cars/:id/repairs/new"><NewRepair /></Route>
+        <Route path="/cars/:id/owner/new"><NewCarOwner /></Route>
+        <Route path="/:username/about"><UserInfo /></Route>
         <Route path= "/"><h1>404 NOT FOUND</h1></Route>
       </Switch>
     </BrowserRouter>
